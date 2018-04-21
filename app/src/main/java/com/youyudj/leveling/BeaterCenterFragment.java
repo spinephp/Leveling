77d596cb57540f360d3dc59337393cafe73c3875
zbd.yiliu <zbd.yiliu@gmail.com>
2018-04-20 20:32:49 -0700

    fix the OutOfMemory crash in BeaterCenterFragment.

--- app/src/main/java/com/youyudj/leveling/BeaterCenterFragment.java
+++ app/src/main/java/com/youyudj/leveling/BeaterCenterFragment.java
@@ -1,5 +1,6 @@
 package com.youyudj.leveling;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -132,6 +133,7 @@ public class BeaterCenterFragment extends Fragment implements View.OnClickListen
                 break;
         }
     }
+    @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -193,7 +195,17 @@ public class BeaterCenterFragment extends Fragment implements View.OnClickListen
                     break;
                 case 6:
                     byte[] data1 = (byte[])msg.obj;
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(data1, 0, data1.length);
+                    // First decode with inJustDecodeBounds=true to check dimensions
+                    final BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inJustDecodeBounds = true;
+                    BitmapFactory.decodeByteArray(data1, 0, data1.length, options);
+
+                    // Calculate inSampleSize
+                    options.inSampleSize = calculateInSampleSize(options, 100, 100);
+
+                    // Decode bitmap with inSampleSize set
+                    options.inJustDecodeBounds = false;
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data1, 0, data1.length, options);
                     activity_open_vip_headicon.setImageBitmap(bitmap);
                     break;
                 default:
@@ -201,6 +213,30 @@ public class BeaterCenterFragment extends Fragment implements View.OnClickListen
             }
         }
     };
+
+    public int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
     @Override
     public void onResume() {
         super.onResume();
